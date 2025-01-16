bf0a47a1a6c17273c9363bd64b3f7f14013bf79c
HARSHINI <harshinisuresh2006@gmail.com>
2025-01-16 07:58:14 +0530

    Add files via upload

---
+++ TECH HOME TASK/JANUARY 2025/13.01.25/2.py
@@ -0,0 +1,11 @@
+ratings = list(map(int, input("Enter the ratings of the children (space-separated): ").split()))
+n = len(ratings)
+candies = [1] * n 
+for i in range(1, n):
+    if ratings[i] > ratings[i - 1]:
+        candies[i] = candies[i - 1] + 1
+for i in range(n - 2, -1, -1):
+    if ratings[i] > ratings[i + 1]:
+        candies[i] = max(candies[i], candies[i + 1] + 1)
+result = sum(candies)
+print(result)
