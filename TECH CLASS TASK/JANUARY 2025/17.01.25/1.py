d72a7542f891faf73c534c8f813060a34b00ff73
HARSHINI <harshinisuresh2006@gmail.com>
2025-01-29 09:12:49 +0530

    Delete TECH CLASS TASK directory

--- TECH CLASS TASK/JANUARY 2025/17.01.25/1.py
+++
@@ -1,6 +0,0 @@
-def reverse_string(s):
-    if len(s) == 0:
-        return s
-    return s[-1] + reverse_string(s[:-1])
-string = input("Enter a string to reverse: ")
-print("Reversed string:", reverse_string(string))
