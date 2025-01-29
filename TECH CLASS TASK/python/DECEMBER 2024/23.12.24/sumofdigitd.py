c20e600ee917cefc132ae4cabe716f3ef81d4769
HARSHINI <harshinisuresh2006@gmail.com>
2025-01-29 09:18:48 +0530

    Delete TECH CLASS TASK directory

--- TECH CLASS TASK/python/DECEMBER 2024/23.12.24/sumofdigitd.py
+++
@@ -1,6 +0,0 @@
-num=int(input("Enter a number:"))
-count=0
-for i in str(num):
-    count+=int(i)
-print(f"the sum of digits in the given number is: {count}")
-#2min
--- TECH CLASS TASK/python/JANUARY 2025/16.01.25/2.py
+++
@@ -1,9 +0,0 @@
-arr = list(map(int, input("Enter the array elements: ").split()))
-seen = set()
-duplicates = set()
-for num in arr:
-    if num in seen:
-        duplicates.add(num)
-    else:
-        seen.add(num)
-print("Duplicates are:", list(duplicates))
